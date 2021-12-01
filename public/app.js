4876c9d3556563ac140433e6165c7748dee9b592
Marcos  Gregório <marcosgregs2019@gmail.com>
2021-12-01 12:39:31 -0300

    attPort
    my Port att

--- public/app.js
+++ public/app.js
@@ -10,3 +10,37 @@ links.forEach(link => {
     })
     
 });
+// Criação das janelas dinamica,do project-container
+
+const projectContainer = document.querySelector('.project-container');
+
+// integração do Project.js como o elemento project-container
+ 
+projects.forEach(project =>{
+    projectContainer.innerHTML +=`<div class="project-card" data-tags="#all, ${project.tags}">
+    <img src="img/${project.image}" alt="">
+    <div class="content">
+        <h1 class="project-nameP">${project.name}</h1>
+        <span class="tags">${project.tags}</span>
+    </div>
+</div>`;
+
+})
+// filtros
+const filters = document.querySelectorAll('.filter-btn');
+
+filters.forEach(filterBtn =>{
+    filterBtn.addEventListener('click', () =>{
+        let id = filterBtn.getAttribute('id');
+        let projectCards = document.querySelectorAll('.project-card');
+        projectCards.forEach(card =>{
+            if(card.getAttribute('data-tags').includes(id)){
+                card.classList.remove('hide');
+            }   else{
+                card.classList.add('hide')
+            }
+        })
+        filters.forEach(btn => btn.classList.remove('active'));
+        filterBtn.classList.add('active')
+    })
+})
---
+++ public/project.js
@@ -0,0 +1,10 @@
+// Criando uma lista de objetos,aonde contem meu projetos,com as tag e imag
+let projects =[
+    {
+        name:"project Ide",
+        tags:"#javascript",
+        image:"ide-project.png"
+
+    }
+]
+
